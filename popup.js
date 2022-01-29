aae66497a9546100c601c685d98fb8f3111ea0b7
Hao <74541600+haod98@users.noreply.github.com>
2022-01-29 10:59:12 +0100

    Fix bug fast forward bug

--- popup.js
+++ popup.js
@@ -4,19 +4,20 @@ const error_msg = document.querySelector('.errorMsg');
 const settings = document.querySelector('.settings');
 btn.addEventListener('click', () => {
     input.classList.remove('error');
+    let timeValue = document.querySelector('#time').value;
     removeErrorMessage();
     if (!timeValue.match(/^[0-9]+$/)) { //Check if input is an int
         input.classList.add('error');
         createErrorMessage();
         return;
-    }
-    sendTimeToContentSide();
+    };
+    sendTimeToContentSide(timeValue);
 });
 
-const sendTimeToContentSide = () => {
-    let timeValue = document.querySelector('#time').value;
+const sendTimeToContentSide = (time) => {
+    console.log(time);
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { getTime: timeValue }, (response) => {
+        chrome.tabs.sendMessage(tabs[0].id, { getTime: time }, (response) => {
             console.log(response.updatedTime);
         });
     });
@@ -27,9 +28,9 @@ const createErrorMessage = () => {
     p.textContent = "Please use a number (seconds)"
     p.classList.add('error-msg');
     settings.appendChild(p);
-}
+};
 
 const removeErrorMessage = () => {
     const errorMsg = document.querySelector(".error-msg");
     if (errorMsg) errorMsg.remove();
-}
+};
